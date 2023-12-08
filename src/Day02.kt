977ee42c3d3bdfc68046bf6dbf108237751fa0ee
Bugi Goertz <burkhard.goertz@shapefruit.de>
2023-12-08 18:26:38 +0100

    solves aoc day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,18 +3,37 @@ fun main() {
 
     fun part1(line: List<String>): Int {
 
-        return 1
+        val test =line.map {
+            it.split("x")
+                    .map { it.toInt() }
+        }.map {
+            (it[0] * it[1] * it[2] / it.max()
+                    + 2 * it[0] * it[1]
+                    + 2 * it[0] * it[2]
+                    + 2 * it[1] * it[2])
+        }.sum()
+
+        return test
 
     }
 
     fun part2(line: List<String>): Int {
 
-        return -1
+        return line.map {
+            it.split("x")
+                    .map { it.toInt() }
+        }.map {
+            (2 * (it[0] + it[1] + it[2] - it.max())
+             + it[0] * it[1] * it[2])
+        }.sum()
+
     }
 
-    // test if implementation meets criteria from the description, like:
+// test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 101)
+    check(part2(testInput) == 48)
+
 
     val input = readInput("Day02")
     part1(input).println()
