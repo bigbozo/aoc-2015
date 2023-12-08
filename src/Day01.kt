e38488724f2ab4eff9f44245c965a43a6527d693
Bugi Goertz <burkhard.goertz@shapefruit.de>
2023-12-08 17:43:01 +0100

    solves aoc 2015 day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun targetFloor(test: String) = test.count { it == '(' } - test.count { it == ')' }
+
+    fun part1(line: List<String>): Int {
+
+        val test = line.first()
+        return targetFloor(test)
+
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(line: List<String>): Int {
+        val test = line.first()
+        for (i in test.indices) {
+            if (targetFloor(test.substring(0, i)) == -1) {
+                return i
+            } else {
+//                print(targetFloor(test.substring(0, i)))
+            }
+        }
+        return -1
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
 
     val input = readInput("Day01")
     part1(input).println()
