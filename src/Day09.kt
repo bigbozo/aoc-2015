14c93a2fd8048d4b4d6901b401c1c75dbf8669fe
Bugi Goertz <burkhard.goertz@shapefruit.de>
2023-12-11 19:58:45 +0100

    Refactor Day09.kt for code simplification
    Revised the implementation of Day09.kt to improve its readability and efficiency. Updated several functions within the code to utilize more streamlined methods such as 'getOrPut' and 'zipWithNext'. Also, redundant variable declarations are omitted for a cleaner look.

--- src/Day09.kt
+++ src/Day09.kt
@@ -7,39 +7,29 @@ fun main() {
         lines.filter { it.isNotEmpty() }.map {
             val match = splitterRegex.find(it)!!.groupValues
             // Add new nodes
-            if (nodes[match[1]] == null) nodes[match[1]] = match[1]
-            if (nodes[match[2]] == null) nodes[match[2]] = match[2]
+            nodes.getOrPut(match[1]) { match[1] }
+            nodes.getOrPut(match[2]) { match[2] }
             // Add edges
             edges[match[1] + '-' + match[2]] = match[3].toInt()
             edges[match[2] + '-' + match[1]] = match[3].toInt()
         }
-        val nodeList = nodes.values.toList()
-        val routes = permutations(nodeList)
-        return Pair(edges, routes)
+        return Pair(edges, permutations(nodes.values.toList()))
     }
 
-    fun measureRoutes(routes: List<List<String>>, edges: HashMap<String, Int>): List<Int> {
-        return routes.map { it ->
-            val route = it.toMutableList()
-            var current = route.removeFirst()
-            var distance = 0
-            route.map { leg ->
-                distance += edges["$current-$leg"]!!
-                current = leg
-            }
-            distance
+    fun measureRoutes(routes: List<List<String>>, edges: HashMap<String, Int>): List<Int> =
+        routes.map { route ->
+            route.zipWithNext { a, b -> edges["$a-$b"] ?: 0 }.sum()
         }
-    }
 
-    fun part1(lines: List<String>): Int {
-        val (edges, routes) = parseData(lines)
-        return measureRoutes(routes, edges).min()
-    }
+    fun part1(lines: List<String>): Int =
+        parseData(lines).let { (edges, routes) ->
+            measureRoutes(routes, edges).min()
+        }
 
-    fun part2(lines: List<String>): Int {
-        val (edges, routes) = parseData(lines)
-        return measureRoutes(routes, edges).max()
-    }
+    fun part2(lines: List<String>): Int =
+        parseData(lines).let { (edges, routes) ->
+            measureRoutes(routes, edges).max()
+        }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
