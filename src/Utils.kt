ff80be76720298f8887136dd72e44b02975beb03
Bugi Goertz <burkhard.goertz@shapefruit.de>
2023-12-11 19:40:21 +0100

    permutations function

--- src/Utils.kt
+++ src/Utils.kt
@@ -21,3 +21,25 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T>permutations(items: List<T>): List<List<T>> {
+
+    // Single Item has only one permutation
+    if (items.size==1) return listOf(items)
+
+    val resultList = mutableListOf<List<T>>()
+
+    var count =0;
+    for (item in items) {
+        val subItems = items.toMutableList()
+        subItems.removeAt(count++)
+        val subItemLists = permutations(subItems)
+        for (subItemList in subItemLists) {
+            val subItem = subItemList.toMutableList()
+            subItem.add(item)
+            resultList.add(subItem)
+        }
+    }
+
+    return resultList
+}
